b48ed3f7199ef57bcbfc1b392fbe027d11340426
dionubt <153142012+dionubt@users.noreply.github.com>
2024-12-25 20:13:34 +0100

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,28 +1,10 @@
 import React from "react";
-import "./App.css"; // Ensure to create and style this CSS file
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const App = () => {
-  return (
-    <div className="app-container">
-      {/* Navbar */}
-      <nav className="navbar">
-        <div className="logo">MyLogo</div>
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#login">Login</a></li>
-          <li><a href="#signup">Sign Up</a></li>
-        </ul>
-      </nav>
-
-      {/* Main Content */}
-      <div className="main-content">
-        <h1>Welcome to Our Website</h1>
-        <p>Your journey starts here. Explore our services and join us today!</p>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
