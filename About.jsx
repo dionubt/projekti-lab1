7d32574eeddb8d4da9f4eb15599a34f1d9bd3cde
dionubt <153142012+dionubt@users.noreply.github.com>
2024-12-27 20:29:47 +0100

    Add files via upload

--- About.jsx
+++ About.jsx
@@ -11,7 +11,9 @@ function About() {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/services">Services</Link></li>
-          <li><Link to="/login">Login</Link></li> {/* Ensure the Login link works */}
+          <li>|</li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Sign up</Link></li>
         </ul>
       </nav>
 
